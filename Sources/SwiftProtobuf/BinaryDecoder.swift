d80a9a7afa9b4b7ec253ece27008f67081f44e17
Mustafa Merza <mustafa.merza95@gmail.com>
2026-03-02 01:35:19 +0300

    - Removed some logging.

--- Sources/SwiftProtobuf/BinaryDecoder.swift
+++ Sources/SwiftProtobuf/BinaryDecoder.swift
@@ -897,11 +897,8 @@ internal struct BinaryDecoder: Decoder {
 
     internal mutating func decodeFullMessage<M: Message>(message: inout M) throws {
         assert(unknownData == nil)
-        print("decodeFullMessage")
         try incrementRecursionDepth()
-        print("incrementRecursionDepth")
         try message.decodeMessage(decoder: &self)
-        print("message.decodeMessage")
         decrementRecursionDepth()
         
         guard complete else {
